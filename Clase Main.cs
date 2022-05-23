6fb0356fa2f720ab89015ba7266070afa8f0ca04
estebantjx <104031838+estebantjx@users.noreply.github.com>
2022-05-23 13:52:21 -0500

    Add files via upload

--- Clase Main.cs
+++ Clase Main.cs
@@ -4,131 +4,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proyecto_definitivo
+namespace proyecto_definitivo
 {
-    public class ClaseMain
+    public class Clase_Main
+    
     {
         public static void Main()
         {
-            int menu;
-            Class_menu.menu();
-            menu = Convert.ToInt32(Console.ReadLine());
-            switch (menu)
+            do
             {
-                case 1:
-                    Operadores.Operadores_1();
-                    int menu1;
-                    menu1 = Convert.ToInt32(Console.ReadLine());
-                    switch (menu1)
+                try
+                {
+                    double menu_1;
+                    Clase_menu.menu();
+                    menu_1 = Convert.ToDouble(Console.ReadLine());
+                    switch (menu_1)
                     {
                         case 1:
-                            Operadores.op_1(1, 2);
-                            break;
+                            Class_Operadores.Op_menu();
+                            double menu_2 = Convert.ToDouble(Console.ReadLine());
+                            switch (menu_2)
+                            {
+                                case 1:
+                                    Class_Operadores.Op_1(1, 2);
+                                    break;
 
-                        case 2:
-                            Operadores.op_2(1, 2);
+                                case 2:
+                                    Class_Operadores.Op_2(1, 2);
+                                    break;
 
-                            break;
-                        case 3:
-                            Operadores.op_3(1);
-                            break;
+                                case 3:
+                                    Class_Operadores.Op_3(1);
+                                    break;
 
-                        case 4:
-                            Operadores.op_4(1, 2);
-                            break;
+                                case 4:
+                                    Class_Operadores.Op_4(1);
+                                    break;
 
-                        case 5:
-                            Operadores.op_5(1);
-                            break;
-                        case 6:
-                            Operadores.op_6(1);
-                            break;
+                                case 5:
+                                    Class_Operadores.Op_5(1);
+                                    break;
+
+                                case 6:
+                                    Class_Operadores.Op_6(1, 2);
+                                    break;
+
                                 case 7:
-                            Operadores.op_7(1);
-                            break;
-                        case 8:
-                            Operadores.op_8(1, 2, 3);
-                            break;
-                        case 9:
-                            Salir.Salir_1();
+                                    Class_Operadores.Op_7(1);
+                                    break;
+
+                                case 8:
+                                    Class_Operadores.Op_8(1, 2, 3);
+                                    break;
+
+                                case 9:
+                                    Class_Operadores.Op_def();
+                                    break;
+                            }
                             break;
 
-                        
-
-                    }
-                    break;
-                case 2:
-                    Condicionales.Condicionales_1();
-                    int menu2;
-                    menu2 = Convert.ToInt32(Console.ReadLine());
-                    switch (menu2) 
-                    {
-                        case 1:
-                            Condicionales.co_1(1);
-
-                            break;
                         case 2:
-                            Condicionales.co_2(1, 2);
+                            Class_Condicionales.Op_menu();
+                            double menu_3 = Convert.ToDouble(Console.ReadLine());
+                            switch (menu_3)
+                            {
+                                case 1:
+                                    Class_Condicionales.Op_1(1);
+                                    break;
+
+                                case 2:
+                                    Class_Condicionales.Op_2(1, 2);
+                                    break;
+
+                                case 3:
+                                    Class_Condicionales.Op_3(1, 2, 3);
+                                    break;
+
+                                case 4:
+                                    Class_Condicionales.Op_4(1, 2);
+                                    break;
+
+                                case 5:
+                                    Class_Condicionales.Op_5(1, 2);
+                                    break;
+
+                                case 6:
+                                    Class_Condicionales.Op_6(1, 2);
+                                    break;
+
+                                case 7:
+                                    Class_Condicionales.Op_7(1);
+                                    break;
+
+                                case 8:
+                                    Class_Condicionales.Op_def();
+                                    break;
+
+                            }
                             break;
+
                         case 3:
-                            Condicionales.co_3(1,2,3);
-                            break;
-                        case 4:
-                            Condicionales.co_4(1, 2);
-                            break;
-                        case 5:
-                            Condicionales.co_5(1,2);
-                            break;
-                        case 6:
-                            Condicionales.co_6(1,2);
-                            break;
-                        case 9:
-                            Salir.Salir_1();
-                            break;
-                    
-                    
-                    }
-                    break;
-                case 3:
-                    Ciclos.Ciclos_1();
-                    int menu3;
-                    menu3 = Convert.ToInt32(Console.ReadLine());
-                    switch (menu3) 
-                    {
-                        case 1:
-                            Ciclos.Ci_1(1);
-                            break;
-                        case 2:
-                            Ciclos.ci_2(1);
-                            break;
-                        case 3:
-                            Ciclos.ci_3(1);
-                            break;
-                        case 4:
-                            Ciclos.ci_4(1);
-                            break;
-                        case 5:
-                            Ciclos.ci_5(1);
-                            break;
-                        case 6:
-                            Ciclos.ci_6(1,2,3);
-                            break;
-                        case 7:
-                            Ciclos.ci_7(1,2);
+                            Class_Condicionales.Op_menu();
+                            double menu_4 = Convert.ToDouble(Console.ReadLine());
+                            switch (menu_4)
+                            {
+                                case 1:
+                                    Class_Ciclos.Op_1(1);
+                                    break;
+
+                                case 2:
+                                    Class_Ciclos.Op_2(1);
+                                    break;
+
+                                case 3:
+                                    Class_Ciclos.Op_3(1);
+                                    break;
+
+                                case 4:
+                                    Class_Ciclos.Op_4(1);
+                                    break;
+
+                                case 5:
+                                    Class_Ciclos.Op_5(1, 2, 3);
+                                    break;
+
+                                case 6:
+                                    Class_Ciclos.Op_6(1, 2, 3);
+                                    break;
+
+                                case 7:
+                                    Class_Ciclos.Op_7(1, 2);
+                                    break;
+
+                                case 8:
+                                    Class_Ciclos.Op_def();
+                                    break;
+                            }
                             break;
 
                         case 9:
-                            Salir.Salir_1();
                             break;
-                    
-                    
+
+                        default:
+                            break;
                     }
-
-                    break;
-
-
-            }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("El termino ingresado no es valido");
+                }
+                
+                Console.WriteLine("Presione ENTER para volver");
+                Console.ReadKey();
+                Console.Clear();
+            } while (true);
 
         }
+
     }
 }
